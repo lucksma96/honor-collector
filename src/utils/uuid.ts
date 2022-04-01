e14025fd17f3b1d3355343f0827efa858b555d72
lucksma96 <lucasmagno96@hotmail.com>
2022-04-01 18:11:53 -0300

    created uuid generation function

---
+++ src/utils/uuid.ts
@@ -0,0 +1,16 @@
+export function uuidv4(): string {
+    return (`${1e7}-${1e3}-${4e3}-${8e3}-${1e11}`).replace(
+        /[018]/g,
+        (c: any) => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+function uuidv6(): string {
+    throw new Error("Not implemented");
+}
+
+export default function uuid(version: 4 | 6): string {
+    if (version == 4) return uuidv4();
+    else if (version == 6) return uuidv6();
+    else return uuidv4();
+}
