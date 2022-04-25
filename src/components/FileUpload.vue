9da934b0df87989cf3a6cde093b1b9a9e8124387
lucksma96 <lz.magnanimous@gmail.com>
2022-04-25 00:43:02 -0300

    FileUpload with vuetify

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,21 +1,33 @@
 <template>
-    <input v-on:change="onFileChange" type="file" multiple accept="image/*" :disabled="loading" />
-
-    <div class="file-list">
-        <span class="image-counter">{{ files.length }} image{{
-            files.length > 1 || files.length == 0 ? "ns" : "m"
-        }}</span>
-        <div v-for="file in files" :key="file.name" class="file-list-item">
-            <img :src="createImgSrc(file)" width="64" height="64" class="file-list-item-image" @load="resizeImage" />
-            <span class="file-list-item-name">{{ file.name }}</span>
-            <button @click="removeFile(file.name)" class="file-list-item-remove">&#x2715</button>
-        </div>
-    </div>
+    <input ref="fileInput" v-on:change="onFileChange" type="file" multiple accept="image/*" :disabled="loading"
+        hidden />
+    <v-btn @click="$refs.fileInput.click()" prepend-icon="mdi-camera" class="mb-4">Escolher imagens</v-btn>
+    <v-card class="my-2">
+        <v-list>
+            <v-list-subheader>
+                {{ files.length }} image{{ files.length > 1 || files.length == 0 ? "ns" : "m" }}
+            </v-list-subheader>
+            <v-list-item v-for="file in files" :key="file.name" :title="file.name">
+                <template v-slot:prepend>
+                    <v-list-item-avatar start>
+                        <img :src="createImgSrc(file)" width="64" height="64" @load="resizeImage" />
+                    </v-list-item-avatar>
+                </template>
+                <template v-slot:append>
+                    <v-list-item-action end>
+                        <v-btn icon="mdi-delete" density="comfortable" @click="removeFile(file.name)"></v-btn>
+                    </v-list-item-action>
+                </template>
+            </v-list-item>
+        </v-list>
+    </v-card>
     <div class="status-message" v-if="isSuccess || isError">
         <span class="success" v-if="isSuccess">Imagens enviadas!</span>
         <span class="error" v-if="isError">Parece que algo deu errado...</span>
     </div>
-    <button @click="send" :disabled="loading || files.length <= 0" class="send-button">{{ sendButtonText }}</button>
+    <v-btn @click="send" :disabled="loading || files.length <= 0" append-icon="mdi-send" class="mt-4">
+        {{ sendButtonText }}
+    </v-btn>
 </template>
 <script lang="ts">
 import resizeImageFile from '@/utils/image-resize';
@@ -80,92 +92,3 @@ export default defineComponent({
     }
 })
 </script>
-<style>
-input[type="file"] {
-    /* display: none; */
-    color: transparent;
-}
-
-input[type="file"]::-webkit-file-upload-button {
-    /* visibility: hidden; */
-    height: 32px;
-    display: inline-block;
-    background: #ed1d24;
-    border: 1px solid #fff100;
-    border-radius: 5px;
-    padding: 5px 8px;
-    outline: none;
-    white-space: nowrap;
-    -webkit-user-select: none;
-    cursor: pointer;
-    text-shadow: none;
-    text-transform: capitalize;
-    font-weight: bold;
-    font-size: 10pt;
-    font-variant: small-caps;
-}
-
-input[type="file"]:hover::before {
-    border-color: black;
-}
-
-.file-list {
-    display: flex;
-    flex-direction: column;
-    background: darkolivegreen;
-    margin: 16px 0 0 0;
-    border-radius: 2px;
-}
-
-.image-counter {
-    margin: 0 4px 0 4px;
-}
-
-.file-list-item {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: 0 5px 5px 5px;
-    padding: 2px;
-    background: rgb(132, 166, 72);
-    border-radius: 2px;
-}
-
-.file-list-item:first-child {
-    margin-top: 5px;
-}
-
-.file-list-item::after {
-    display: block;
-    border: 3px solid lightblue;
-}
-
-.file-list-item-image {
-    border-radius: 2px;
-}
-
-.file-list-item-name {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    margin: 0 4px 0 4px;
-}
-
-.send-button {
-    height: 32px;
-    width: 100%;
-    margin: 16px 0 8px 0;
-    background: lightblue;
-    border: none;
-    border-radius: 5px;
-}
-
-.status-message .success {
-    color: green;
-    margin: 0 4px 0 4px;
-}
-
-.status-message .error {
-    color: red;
-    margin: 0 4px 0 4px;
-}
-</style>
