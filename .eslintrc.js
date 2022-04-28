2173d293a582bd738ce40cd2df2b4e92499cfc9d
lucksma96 <lz.magnanimous@gmail.com>
2022-04-28 00:41:07 -0300

    changed eslint indent to warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,6 @@ module.exports = {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
         "quote-props": ["error", "consistent"],
-        "indent": ["error", 4],
+        "indent": ["warn", 4],
     },
 }
